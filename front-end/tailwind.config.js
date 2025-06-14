51edd46e9e3753b35734bf51e99c1bd50767c434
Mario <cervinschi13@gmail.com>
2025-06-14 13:08:40 +0300

    added cool navbar

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-nav-via': '#064e3b',
+        'custom-nav-secondary': '#022c22',
+        'custom-nav-primary': '#134e4a',
+        'custom-nav-selected': '#1e2d29',
+        'custom-background': '#f0fdf4',
+        'custom-secondary-background': '#dcfce5',
+        'custom-card-background': '#ffffff',
+        'custom-hover-link': '#064e3b',
+      },
+    },
   },
   plugins: [],
 }
